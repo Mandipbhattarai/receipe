7623f2a030746938d3a3ffff7e8ffe0309f743e7
Mandip Bhattarai <serverany9@gmail.com>
2025-10-14 22:24:05 +0530

    final

--- next.config.mjs
+++ next.config.mjs
@@ -1,40 +1,27 @@
-let userConfig = undefined;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   images: { unoptimized: true },
-  experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: true,
-    parallelServerCompiles: true,
-  },
-  output: "standalone",
   productionBrowserSourceMaps: false,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
+  output: "standalone",
+  experimental: {
+    webpackBuildWorker: false,
+    parallelServerBuildTraces: false,
+    parallelServerCompiles: false,
+  },
+  webpack: (config, { isServer, dev }) => {
+    if (!isServer && !dev) {
       config.optimization.splitChunks = {
         chunks: "all",
-        maxSize: 250000,
+        minSize: 300000,
+        maxInitialRequests: 5,
+        maxAsyncRequests: 10,
       };
     }
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.cache = { type: "filesystem", compression: "gzip" };
     return config;
   },
 };
-
-if (userConfig) {
-  const config = userConfig.default || userConfig;
-  for (const key in config) {
-    if (
-      typeof nextConfig[key] === "object" &&
-      !Array.isArray(nextConfig[key])
-    ) {
-      nextConfig[key] = { ...nextConfig[key], ...config[key] };
-    } else {
-      nextConfig[key] = config[key];
-    }
-  }
-}
-
 export default nextConfig;
